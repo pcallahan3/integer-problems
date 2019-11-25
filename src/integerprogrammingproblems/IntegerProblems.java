9684be9be0af8d452a5f3f1e45e71de85dbbe99a
Patrick <pcallahan3@mail.greenriver.edu>
2019-11-25 15:58:52 -0800

    Wrote problem statement and pseudocode for sumArray() function

--- src/integerprogrammingproblems/IntegerProblems.java
+++ src/integerprogrammingproblems/IntegerProblems.java
@@ -9,4 +9,15 @@ public class IntegerProblems {
 
 	}
 
+	
+	
+	
+	/*Problem statement: Sum an array of integers 
+	 *Pseudocode for sumArray() function: 
+	 *     1. Declare and initialize a variable sum to store the sum of the array
+	 *     2. Loop over the length of the array and increment index 
+	 *     3. Sum element of the array indexes
+	 *     4. Return the sum of the elements
+	 * 
+	 */
 }
