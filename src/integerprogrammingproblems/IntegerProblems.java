1b78e5dae09ca392cf175181f9e46988dd2cb7a0
Patrick <pcallahan3@mail.greenriver.edu>
2019-11-25 16:48:45 -0800

    Implemented sumArray() function to sum an array integers

--- src/integerprogrammingproblems/IntegerProblems.java
+++ src/integerprogrammingproblems/IntegerProblems.java
@@ -6,18 +6,26 @@ public class IntegerProblems {
 
 	public static void main(String[] args) {
 		
-
+		int[] arr = {1,23,2,24,3};
+		System.out.println(sumArray(arr));
 	}
 
 	
-	
-	
-	/*Problem statement: Sum an array of integers 
-	 *Pseudocode for sumArray() function: 
-	 *     1. Declare and initialize a variable sum to store the sum of the array
+	/* Problem statement: Sum an array of integers 
+	 * Pseudocode for sumArray() function: 
+	 *     1. Declare and initialize a variable sum to 0 to store the sum of the array
 	 *     2. Loop over the length of the array and increment index 
 	 *     3. Sum element of the array indexes
 	 *     4. Return the sum of the elements
-	 * 
 	 */
+	
+	static int sumArray(int[] arr) {
+		
+		int sum = 0;
+		for(int i = 0;i < arr.length;i++) {
+			sum += arr[i];
+		}
+		
+		return sum;
+	}
 }
