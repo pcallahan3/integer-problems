13bb744339384b1b8b5644aee097e4c8920115bb
Patrick <pcallahan3@mail.greenriver.edu>
2019-11-26 01:47:15 -0800

    Calculated space and time complexity on sumArarry() function

--- src/integerprogrammingproblems/IntegerProblems.java
+++ src/integerprogrammingproblems/IntegerProblems.java
@@ -11,21 +11,42 @@ public class IntegerProblems {
 	}
 
 	
-	/* Problem statement: Sum an array of integers 
-	 * Pseudocode for sumArray() function: 
-	 *     1. Declare and initialize a variable sum to 0 to store the sum of the array
-	 *     2. Loop over the length of the array and increment index 
-	 *     3. Sum element of the array indexes
-	 *     4. Return the sum of the elements
+	/* Problem statement: Sum an array of integers.
+	 * Pseudocode for sumArray() function:
+	 *     1. Create a function that takes one array argument to sum
+	 *     2. Declare and initialize a variable sum to 0 to store the sum of the array
+	 *     3. Loop over the length of the array and increment index 
+	 *     4. Sum element of the array indexes
+	 *     5. Return the sum of the elements
+	 *     
+	 *  Space and time complexity of f = O(1 + 1 + 1) + O(n) = O(n)
 	 */
-	
 	static int sumArray(int[] arr) {
-		
-		int sum = 0;
-		for(int i = 0;i < arr.length;i++) {
-			sum += arr[i];
+		int sum = 0;// O(1) constant time operation
+		for(int i = 0;i < arr.length;i++) {//O(n) linear time operation 
+			sum += arr[i];//O(1) constant time operation 
 		}
 		
-		return sum;
+		return sum;//O(1) constant time operation
 	}
+	
+	/* Problem statement: Find if an element exists in an array of integers if element doesnt exist throw 
+	 *                    an error.
+	 * Pseudocode for findElementInArray() function: 
+	 *     1. Create a function that takes in 2 arguments one which is an array to search, 2nd argument for a 
+	 *        key element to find.
+	 *     2. Loop over the length of the array and increment the index
+	 *     3. Check if key is 
+	 * 
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
