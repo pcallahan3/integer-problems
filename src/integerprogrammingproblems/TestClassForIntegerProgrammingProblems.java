8daa3e11b6a12a5eb8a44154287a72d06a83b077
Patrick <pcallahan3@mail.greenriver.edu>
2019-12-11 16:28:17 -0800

    Created a unit test for findElementInArray() function that passed

--- src/integerprogrammingproblems/TestClassForIntegerProgrammingProblems.java
+++ src/integerprogrammingproblems/TestClassForIntegerProgrammingProblems.java
@@ -7,13 +7,21 @@ import org.junit.Test;
 //A class to test IntegerProblems class
 public class TestClassForIntegerProgrammingProblems {
 	
-	
-	
+	//Unit test for sumArray() function 
 	@Test
 	public void sumArrayUnitTest() {
 		IntegerProblems sumArrayTest = new IntegerProblems();
 		int[] testArr = {1,2,3};
 		assertEquals(6, sumArrayTest.sumArray(testArr));
+		
+	}
+	
+	//Unit test for findElementInArray
+	@Test
+	public void findElementInArrayUnitTest() {
+		IntegerProblems findElementInArrayTest = new IntegerProblems();
+		int[] testArr = {23,7,83,12,98,1};
+		assertEquals(7, findElementInArrayTest.findElementInArray(testArr, 7));
 		
 	}
 	
